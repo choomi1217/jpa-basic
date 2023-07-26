e22052a946fcf51e2c33baed42247df2a90dc38e
o_omi1217 <whdudal1217@naver.com>
2023-07-26 23:59:22 +0900

    Initial commit

---
+++ src/main/java/org/example/member/Main.java
@@ -0,0 +1,42 @@
+package org.example.member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("myApp");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        logic(em);
+        tx.commit();
+
+        emf.close();
+    }
+
+    private static void logic(EntityManager em) {
+
+        Member member = new Member();
+        member.setName("cho");
+        member.setAge(10);
+
+        em.persist(member);
+
+        member.setAge(20);
+        Long id = member.getId();
+
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember = " + findMember.getName() + ", age = " + findMember.getAge());
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        System.out.println("members.size() = " + members.size());
+
+        em.remove(member);
+    }
+}
---
+++ src/main/java/org/example/member/Member.java
@@ -0,0 +1,38 @@
+package org.example.member;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "member")
+public class Member {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column
+    private String name;
+
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
