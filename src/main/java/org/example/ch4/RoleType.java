4e81477c3f76b1c7ed2ac5cbd565efe75f7ac145
o_omi1217 <whdudal1217@naver.com>
2023-08-01 14:07:29 +0900

    :sparkles:

--- src/main/java/org/example/ch4/RoleType.java
+++
@@ -1,5 +0,0 @@
-package org.example.ch4;
-
-public enum RoleType {
-    ADMIN, USER
-}
--- src/main/java/org/example/ch4/_1_SequenceIncrement.java
+++ src/main/java/org/example/ch4/_1_SequenceIncrement.java
@@ -1,9 +1,10 @@
 package org.example.ch4;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
 
 public class _1_SequenceIncrement {
     public static void main(String[] args) {
@@ -37,4 +38,83 @@ public class _1_SequenceIncrement {
         emf.close();
 
     }
+
+}
+@Entity @Getter @Setter
+@Table(name = "identity_user", uniqueConstraints = {@UniqueConstraint(
+        name = "NAME_AGE_UNIQUE",
+        columnNames = {"username", "age"}
+)})
+class IdentityUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, length = 20)
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+}
+
+@Entity @Getter @Setter
+@Table(name = "sequence_user")
+@SequenceGenerator(
+        name = "USER_SEQ_GENERATOR",
+        sequenceName = "SEQUENCE_USER_SEQ",
+        initialValue = 1, allocationSize = 1
+)
+class SequenceUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_SEQ_GENERATOR")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+}
+
+@Entity @Getter @Setter
+@TableGenerator(
+        name = "TABLE_USER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "USER_SEQ", allocationSize = 1
+)
+class TableUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "TABLE_USER_SEQ_GENERATOR")
+    private Long id;
+    private String username;
+    private Integer age;
+    private RoleType roleType;
+    private String description;
+}
+
+enum RoleType {
+    ADMIN, USER
 }
