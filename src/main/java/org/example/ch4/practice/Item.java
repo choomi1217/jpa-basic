7136d953a0d9a763e02c4fcfc95167d38ff34a87
o_omi1217 <whdudal1217@naver.com>
2023-08-01 16:53:25 +0900

    :sparkles:

---
+++ src/main/java/org/example/ch4/practice/Item.java
@@ -0,0 +1,49 @@
+package org.example.ch4.practice;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item_practice")
+public class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+    private int stockQuantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
---
+++ src/main/java/org/example/ch4/practice/OrderItem.java
@@ -0,0 +1,61 @@
+package org.example.ch4.practice;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item_practice")
+public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @Column(name = "item_id")
+    private Long itemId;
+
+    @Column(name = "order_id")
+    private Long orderId;
+
+    private int orderPrice;
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
